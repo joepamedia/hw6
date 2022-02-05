85a10c3eb7601bb6b4be0c25ade14b4b4861c9b3
joepamedia <josephpalmisano18@gmail.com>
2022-02-05 14:19:29 -0600

    added localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // variables
-const searchCities = [];
+const searchCities = JSON.parse(localStorage.getItem("cityHistory")) || [];
 // functions
 function handleCoords(searchCity) {
   const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=4b9f7dc3f8536150bc0eb915e8e4a81b`;
@@ -38,6 +38,26 @@ function handleCurrentWeather(coordinates, city) {
     });
 }
 
+function loadHistoryBtn() {
+  // we want to access local storage
+  // then we want to parse through the string to get the array of objects
+  const history = JSON.parse(localStorage.getItem("cityHistory"));
+  if (history === null) {
+    return;
+  }
+  // then we want to display them on the page when the user refreshes or loads page. (call this function in global scope)
+  console.log(history);
+  displayHistoryBtn(history);
+}
+
+function displayHistoryBtn(array) {
+  console.log(array);
+  array.forEach((city) => {
+    console.log(city);
+    document.querySelector("#searchHistory").innerHTML += `<button data-city="${city}" class="w-100 d-block my-2 btn-secondary history-btn">${city}</button>`;
+  });
+}
+
 function displayCurrentWeather(currentCityData, cityName) {
   // shows current city and weather
   let weatherIcon = `https://openweathermap.org/img/wn/${currentCityData.weather[0].icon}.png`;
@@ -50,7 +70,7 @@ function displayCurrentWeather(currentCityData, cityName) {
     uvClass = "high";
   }
   // shows city, temp, icon, wind, humidity, and UV index
-  document.querySelector("#currentWeather").innerHTML = `<h2>${cityName} ${moment.unix(currentCityData.dt).format("MMM Do YY")} <img src="${weatherIcon}"></h2> <div>Temp: ${currentCityData.temp} \xB0F <br></br> Wind: ${currentCityData.wind_speed} MPH <br></br> Humidity: ${currentCityData.humidity} % <br></br> UV Index: <span class="${uvClass}">${currentCityData.uvi}</span></div>`;
+  document.querySelector("#currentWeather").innerHTML = `<div class="card m-1 p-2"><h2>${cityName} ${moment.unix(currentCityData.dt).format("MMM Do YY")} <img src="${weatherIcon}"></h2> <div>Temp: ${currentCityData.temp} \xB0F <br></br> Wind: ${currentCityData.wind_speed} MPH <br></br> Humidity: ${currentCityData.humidity} % <br></br> UV Index: <span class="${uvClass}">${currentCityData.uvi}</span></div></div>`;
 }
 
 function displayFiveDayWeather(fiveDayCityData) {
@@ -73,11 +93,10 @@ function handleFormSubmit(event) {
   searchCities.push(city.toUpperCase());
   // allows buttons to not duplicate cities that are already searched
   const filteredCities = searchCities.filter((city, index) => {
-    return searchCities.indexOf(city) === index;
+    return searchCities.indexOf(city.toUpperCase()) === index;
   });
-  filteredCities.forEach((city) => {
-    document.querySelector("#searchHistory").innerHTML += `<button data-city="${city}" class="w-100 d-block my-2 btn-secondary history-btn">${city}</button>`;
-  });
+  localStorage.setItem("cityHistory", JSON.stringify(filteredCities));
+  displayHistoryBtn(filteredCities);
 
   handleCoords(city);
 }
@@ -87,6 +106,7 @@ function handleHistory(event) {
   handleCoords(city);
 }
 
+displayHistoryBtn(searchCities);
 // listeners
 // on page load, show any past cities searched
 // search for city
